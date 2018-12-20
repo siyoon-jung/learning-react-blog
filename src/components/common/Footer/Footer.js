567677858fdddb5c8e06bba790d114ce2aecbef3
USER <user@AL01151527.local>
2018-12-20 12:33:41 +0900

    컴포넌트 생성

---
+++ src/components/common/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classnames from 'classnames';
+import styles from './Footer.scss';
+
+const Footer = () => (
+  <div>
+    Footer
+  </div>
+);
+
+export default Footer;
---
+++ src/components/common/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classnames from 'classnames';
+import styles from './Header.scss';
+
+const Header = () => (
+  <div>
+    Header
+  </div>
+);
+
+export default Header;
