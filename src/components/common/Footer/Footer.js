f8c879035670c0e7929a3fa1810cbab28ce45cb9
USER <user@AL01151527.local>
2019-01-03 12:27:04 +0900

    Footer 컴포넌트 생성

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import classnames from 'classnames';
+import {Link} from 'react-router-dom';
+import classNames from 'classnames/bind';
 import styles from './Footer.scss';
 
+const cx = classNames.bind(styles);
+
 const Footer = () => (
-  <div>
-    Footer
-  </div>
+  <footer className={cx('footer')}>
+    <Link to='/' className={cx('brand')}>reactblog</Link>
+    <div className={cx('admin-login')}>관리자 로그인</div>
+  </footer>
 );
 
 export default Footer;
--- src/components/common/PageTemplate.js
+++ src/components/common/PageTemplate.js
@@ -2,12 +2,14 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import Header from './Header/Header';
 import styles from './PageTemplate.scss';
+import Footer from './Footer/Footer';
 
 const cx = classNames.bind(styles);
 
 const PageTemplate = () => (
   <div className={cx('page-template')}>
     <Header />
+    <Footer />
   </div>
 );
 
