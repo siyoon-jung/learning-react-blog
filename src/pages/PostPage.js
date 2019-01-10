bc0b0b2420256af476fd8e63066fc4b4273d273b
USER <user@AL01151527.local>
2019-01-10 12:36:29 +0900

    PostInfo 컴포넌트 생성

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PageTemplate from 'components/common/PageTemplate';
+import PostInfo from 'components/post/PostInfo';
 
 const PostPage = () => {
   return (
-    <div>
-      post
-    </div>
+    <PageTemplate>
+      <PostInfo />
+    </PageTemplate>
   )
 }
 
