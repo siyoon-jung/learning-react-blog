942e768646c6fa061921be57c0a9d136e360925c
USER <user@AL01151527.local>
2019-01-10 12:46:03 +0900

    EditorTemplate 컴포넌트 생성

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import EditorTemplate from 'components/editor/EditorTemplate';
 
 const EditorPage = () => {
   return (
-    <div>
-      editor
-    </div>
+    <EditorTemplate
+      header="header"
+      editor="editor"
+      preview="preview"
+    />
   )
 }
 
